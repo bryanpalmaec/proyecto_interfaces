d95891427170812a0a654ffd0c2ed593671ee2aa
bryanpalmaec <bryan.palma44@gmail.com>
2025-07-17 18:51:35 -0500

    correcciones finales v2

--- src/app/portafolio/portafolio.ts
+++ src/app/portafolio/portafolio.ts
@@ -10,5 +10,5 @@ export class Portafolio {
   titulo = 'Proyectos de Impacto Global';
   descripcion = `Este proyecto representa un esfuerzo colaborativo para transformar digitalmente el ecosistema de salud pública en América Latina. Desarrollamos una plataforma escalable con inteligencia artificial que automatiza procesos clínicos, mejora la accesibilidad a tratamientos y centraliza datos de pacientes con estándares internacionales.`;
 
-  videoUrl = 'https://cdn.pixabay.com/video/2022/10/24/136259-764387660_large.mp4'; // Asegúrate que esté en src/assets/videos/
+  videoUrl = 'https://cdn.pixabay.com/video/2022/10/24/136259-764387660_large.mp4';
 }
